2a9163875978dbefddaed69074c96e439176f0cb
wang zuo <wzuoadjusted@gmail.com>
2015-08-13 13:42:33 +0800

    test with tape

--- test.js
+++ test.js
@@ -1,35 +1,40 @@
-var assert = require('assert');
 var cf = require('./index');
+var test = require('tape');
 
-describe('color functions', function() {
-  it('hex2rgb', function() {
-    assert.deepEqual(cf.hex2rgb('3498db'), {r: 52, g: 152, b: 219});
-    assert.deepEqual(cf.hex2rgb('#3498db'), {r: 52, g: 152, b: 219});
-    assert.deepEqual(cf.hex2rgb('#aabbcc'), {r: 170, g: 187, b: 204});
-    assert.deepEqual(cf.hex2rgb('#abc'), {r: 170, g: 187, b: 204});
-  });
+test('hex2rgb', function(t) {
+  t.plan(4);
+  t.deepEqual(cf.hex2rgb('3498db'), {r: 52, g: 152, b: 219});
+  t.deepEqual(cf.hex2rgb('#3498db'), {r: 52, g: 152, b: 219});
+  t.deepEqual(cf.hex2rgb('#aabbcc'), {r: 170, g: 187, b: 204});
+  t.deepEqual(cf.hex2rgb('#abc'), {r: 170, g: 187, b: 204});
+});
 
-  it('hsv2hex', function() {
-    assert.strictEqual(cf.hsv2hex(204, 76, 86), '3599db');
-  });
+test('hsv2hex', function(t) {
+  t.plan(1);
+  t.strictEqual(cf.hsv2hex(204, 76, 86), '3599db');
+});
 
-  it('hsv2rgb', function() {
-    assert.deepEqual(cf.hsv2rgb(204, 76, 86), {r: 53, g: 153, b: 219});
-  });
+test('hsv2rgb', function(t) {
+  t.plan(1);
+  t.deepEqual(cf.hsv2rgb(204, 76, 86), {r: 53, g: 153, b: 219});
+});
 
-  it('rgb2hex', function() {
-    assert.strictEqual(cf.rgb2hex(52, 152, 219), '3498db');
-  });
+test('rgb2hex', function(t) {
+  t.plan(1);
+  t.strictEqual(cf.rgb2hex(52, 152, 219), '3498db');
+});
 
-  it('rgb2hsv', function() {
-    assert.deepEqual(cf.rgb2hsv(52, 152, 219), {h: 204, s: 76, v: 86});
-  });
+test('rgb2hsv', function(t) {
+  t.plan(1);
+  t.deepEqual(cf.rgb2hsv(52, 152, 219), {h: 204, s: 76, v: 86});
+});
 
-  it('rgba', function() {
-    assert.strictEqual(cf.rgba(52, 152, 219, 70), 'rgba(52,152,219,0.7)');
-  });
+test('rgba', function(t) {
+  t.plan(1);
+  t.strictEqual(cf.rgba(52, 152, 219, 70), 'rgba(52,152,219,0.7)');
+});
 
-  it('css-color', function() {
-    assert.deepEqual(cf.cssColor('hsl(0, 100%, 50%)'), {r: 255, g: 0, b: 0});
-  });
+test('css-color', function(t) {
+  t.plan(1);
+  t.deepEqual(cf.cssColor('hsl(0, 100%, 50%)'), {r: 255, g: 0, b: 0});
 });
